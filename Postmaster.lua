5ea8cad8a69011da0539d54392bdb1ac2e704c1c
silvereyes <silvereyes333@gmail.com>
2019-10-21 12:40:51 -0500

    Version 3.9.2
    - Update 24 Dragonhold API bump

--- Postmaster.lua
+++ Postmaster.lua
@@ -5,7 +5,7 @@
 Postmaster = {
     name = "Postmaster",
     title = GetString(SI_PM_NAME),
-    version = "3.9.1",
+    version = "3.9.2",
     author = "silvereyes, Garkin & Zierk",
     
     -- For development use only. Set to true to see a ridiculously verbose 
